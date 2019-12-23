45eb22f4a75281ef95d7856f9996d4dc34f1385a
Nico Stranquist <nstranquist@gmail.com>
2019-12-22 18:43:58 -0600

    add firebaseConfig.ts to fix build/deploy

---
+++ src/utils/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAAWe6ePqRa0SdoLJxVaGKiXOdK8MHgQ4s",
+  authDomain: "jacobs-dashboard.firebaseapp.com",
+  databaseURL: "https://jacobs-dashboard.firebaseio.com",
+  projectId: "jacobs-dashboard",
+  storageBucket: "jacobs-dashboard.appspot.com",
+  messagingSenderId: "436395913903",
+  appId: "1:436395913903:web:db0f2e73a6fdb7769fda5a",
+  measurementId: "G-3GH9T6Z3JS"
+}
